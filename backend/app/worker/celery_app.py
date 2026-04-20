62c1ecfff52f6fdbf31f32e2a1a6a0feff23a44b
pmwensing <pmwensing1981@gmail.com>
2026-04-19 21:38:41 -0400

    Add Celery app

---
+++ backend/app/worker/celery_app.py
@@ -0,0 +1,12 @@
+from celery import Celery
+from app.core.config import settings
+
+celery_app = Celery(
+    "osint_worker",
+    broker=settings.redis_url,
+    backend=settings.redis_url
+)
+
+celery_app.conf.task_routes = {
+    "app.worker.tasks.*": {"queue": "default"}
+}
